16008e3a202337ac620a0f4819a1619ee471262e
Jorge Luis Monroy <jlmonroy13@gmail.com>
2016-01-28 17:52:36 -0500

    Funciones trigonometricas y operaciones de un paso lista

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ Joonik.com written by - Jorge Luis Monroy. 2016
 var check;
 var sumTotal;
 var sum1;
+var key;
 // Titilar el cursor
 // function titilar () {
 // 	clearInterval(check);
@@ -14,6 +15,22 @@ var sum1;
 $(document).ready(function() { 
 	// titilar();
 	var num = '';
+	//Asignando teclas a botones
+	$("body").keydown(function( event ) {
+		event.preventDefault();
+		if ( event.which == 13 ) {//igual
+			$('.equal').click();
+		}//números
+		else if ( event.which > 47 && event.which < 58) {
+			$('.num' + (event.which - 48)).click();
+		}//borrar último caracter
+		else if ( event.which == 8) {
+			$('.btn-lastChar').click();
+		}
+		else if ( event.which == 27) {
+			$('.btn-clear').click();
+		}
+	});
 	//Mostrar números en el display
 	$('.btn-num').on('click', function() {
 		// clearInterval(check);
@@ -33,6 +50,7 @@ $(document).ready(function() {
 					$('#display span').text('0');
 				}
 			}
+
 			// $('#display span').removeClass('no-show');
 		}//En el caso que se presione un número después de haber presionado 'igual'
 		if ($('.equal').data('id') === 1) {
@@ -67,8 +85,28 @@ $(document).ready(function() {
 			}
 		}
 	});
-	//Sumar
+	//Operaciones un paso 
+	$('.unPaso').on('click', function() {
+		$(this).addClass('operators');
+		$('.equal').click();
+
+	});
+	//Botón euler
+	$('.euler').on('click', function() {
+		num = 2.718281828459045;
+		$('#display span').text(num);
+	});
+	//Botón pi
+	$('.pi').on('click', function() {
+		num = 3.141592653589793;
+		$('#display span').text(num);
+	});
+
+	//Operaciones dos pasos
 	$('.dosPasos').on('click', function() {
+		if ($('.dosPasos').hasClass('operators')) {
+			$('.equal').click();
+		}
 		if ( num === '' || ($('#display span').text() === '0') ) {
 			sum1 = '0';
 			num = "";
@@ -104,9 +142,55 @@ $(document).ready(function() {
 		if( $('.btn-raiz').hasClass('operators') ) {
 			sumTotal = Math.pow( parseFloat(sum1) , 1 / parseFloat($('#display span').text()));
 		}
+		if( $('.unox').hasClass('operators') ) {
+			sumTotal = 1 / parseFloat($('#display span').text());
+		}
+		if( $('.alcuadrado').hasClass('operators') ) {
+			sumTotal = parseFloat($('#display span').text()) * parseFloat($('#display span').text());
+		}
+		if( $('.alcuadrado').hasClass('operators') ) {
+			sumTotal = parseFloat($('#display span').text()) * parseFloat($('#display span').text());
+		}
+		if( $('.alcuadrado').hasClass('operators') ) {
+			sumTotal = parseFloat($('#display span').text()) * parseFloat($('#display span').text());
+		}
+		if( $('.alcubo').hasClass('operators') ) {
+			sumTotal = parseFloat($('#display span').text()) * parseFloat($('#display span').text()) * parseFloat($('#display span').text());
+		}
+		if( $('.factorial').hasClass('operators') ) {
+			sumTotal = 1;
+			for (var i = 1; i <= parseFloat($('#display span').text()); i++) {
+				sumTotal *= i;
+			}
+		}
+		if( $('.sen').hasClass('operators') ) {
+			sumTotal = Math.sin(parseFloat($('#display span').text()));
+		}
+		if( $('.cos').hasClass('operators') ) {
+			sumTotal = Math.cos(parseFloat($('#display span').text()));
+		}
+		if( $('.tan').hasClass('operators') ) {
+			sumTotal = Math.tan(parseFloat($('#display span').text()));
+		}
+		if( $('.ln').hasClass('operators') ) {
+			sumTotal = Math.log(parseFloat($('#display span').text()));
+		}
+		if( $('.log').hasClass('operators') ) {
+			sumTotal = Math.log10(parseFloat($('#display span').text()));
+		}
+		if( $('.eulerx').hasClass('operators') ) {
+			sumTotal = Math.pow( 2.718281828459045 , parseFloat($('#display span').text()));
+		}
+		if( $('.raiz2').hasClass('operators') ) {
+			sumTotal = Math.pow( parseFloat($('#display span').text()) , 1 / 2);
+		}
+		if( $('.porcentaje').hasClass('operators') ) {
+			sumTotal = parseFloat($('#display span').text()) / 100;
+		}
 		num = '' + sumTotal;
 		$('#display span').text(sumTotal);
 		$('.dosPasos').removeClass('operators');
+		$('.unPaso').removeClass('operators');
 		$(this).data('id', 1);	
 	});
 });
